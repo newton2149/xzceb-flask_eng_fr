13feb1599357148c4f434787969d71784f1401a8
newton2149 <navneetsasikumar@gmail.com>
2023-05-09 05:06:28 -0400

    my-repo

--- final_project/server.py
+++ final_project/server.py
@@ -19,6 +19,8 @@ def frenchToEnglish():
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template("index.html")
 
-if __name__ == "__main__":
+
+if __name__=="__main__":
     app.run(host="0.0.0.0", port=8080)
